895b15ad0255d665d10e458a1605675e9252fe9a
GitHub Action <action@github.com>
2022-11-04 12:02:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/abuballan/adbcommanderplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/abuballan/adbcommanderplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.abuballan.adbcommanderplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.abuballan.adbcommanderplugin.MyBundle
 
 class MyApplicationService {
 
